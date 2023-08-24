151d5f05757cc8de6438c25e8b281af63155e9bc
David Andersson <lejonmanen@gmail.com>
2023-08-24 16:13:19 +0200

    Mapbox done

--- mapbox-demo/src/App.tsx
+++ mapbox-demo/src/App.tsx
@@ -1,35 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useRef, useEffect } from 'react'
+import mapboxgl, { Map as MapGl } from 'mapbox-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+// För att TypeScript ska hitta typerna:
+// npm i --save-dev @types/node
+// npm i --save-dev @types/mapbox-gl
+mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_TOKEN as string
+console.log('Kontrollera att access token hittas: ', import.meta.env.VITE_MAPBOX_TOKEN);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+function App() {
+	const mapContainer = useRef(null)
+	const mapRef = useRef<MapGl | null>(null)
+	const [lat, setLat] = useState<number>(57.7)
+	const [lng, setLng] = useState<number>(11.89)
+	const [zoom, setZoom] = useState<number>(10)
+
+	useEffect(() => {
+		if( mapRef.current || !mapContainer.current ) return
+
+		mapRef.current = new MapGl({
+			container: mapContainer.current,
+			style: 'mapbox://styles/mapbox/streets-v12',
+			center: [lng, lat],
+			zoom: zoom
+		});
+		const map: MapGl = mapRef.current
+
+		map.on('move', () => {
+			interface Position {
+				lng: number;
+				lat: number;
+			}
+			const position: Position = map.getCenter()
+			setLat(Number(position.lat.toFixed(4)))
+			setLng(Number(position.lng.toFixed(4)))
+			setZoom(map.getZoom());
+		})
+	}, [lat, lng, zoom])
+
+	return (
+		<div>
+			<header> Mapbox demo </header>
+			<main>
+
+				<div ref={mapContainer} className="map-container" />
+
+				<p> Center position: {lat} lat, {lng} lng </p>
+			</main>
+		</div>
+	)
 }
 
 export default App
